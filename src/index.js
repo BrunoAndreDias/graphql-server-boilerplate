8a1f390431896ab2acd647a6c1cd6e2f94b63a2f
Bruno Dias <baad@sapo.pt>
2019-03-12 21:04:41 +0000

    Add message type and resolver

--- src/index.js
+++ src/index.js
@@ -1,60 +1,80 @@
-import express from 'express';
-import {
-    ApolloServer,
-    gql
-} from 'apollo-server-express';
-import cors from 'cors';
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 
-const schema = gql `
+const schema = gql`
   type Query {
-      users: [User!]
+    users: [User!]
+    user(id: ID!): User
     me: User
-    user (id: ID!): User
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
-      id: ID!
-      username: String!
+    id: ID!
+    username: String!
+  }
+
+  type Message {
+    id: ID!
+    text: String!
   }
 `;
 
 let users = {
-    1: {
-        id: '1',
-        username: 'Robin Wieruch',
-    },
-    2: {
-        id: '2',
-        username: 'Dave Davids',
-    },
-}
-const me = users[1];
+  1: {
+    id: "1",
+    username: "Robin Wieruch"
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids"
+  }
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World"
+  },
+  2: {
+    id: "2",
+    text: "By World"
+  }
+};
 
 const resolvers = {
-    Query: {
-        users: () => Object.values(users),
-        me: () => me,
-        user: (parent, {
-            id
-        }) => users[id],
-    },
+  Query: {
+    users: () => Object.values(users),
+    user: (parent, { id }) => users[id],
+    me: (parent, args, { me }) => me,
+    messages: () => Object.values(messages),
+    message: (parent, { id }) => message[id]
+  }
 };
 
 const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
+  typeDefs: schema,
+  resolvers,
+  context: {
+    me: users[1]
+  }
 });
 
 server.applyMiddleware({
-    app,
-    path: '/graphql'
+  app,
+  path: "/graphql"
 });
 
-app.listen({
+app.listen(
+  {
     port: 8000
-}, () => {
-    console.log('Apollo Server on http://localhost:8000/graphql');
-});
+  },
+  () => {
+    console.log("Apollo Server on http://localhost:8000/graphql");
+  }
+);
